61e36473d05bd864cb144ff317a3a25d7081fbca
rim bouda <rimaarimoo16@gmail.com>
2025-02-10 21:41:05 +0100

    Updated feature X

--- application/routes/web.php
+++ application/routes/web.php
@@ -36,7 +36,7 @@ Route::post('/logout', function() {
 })->name('logout');
 
 
-
+Route::get('/', [HomeController::class, 'index'])->name('home'); 
 Route::get('/about-us', [AboutController::class, 'index'])->name('about.us');
 Route::get('/doctor', [DoctorController::class, 'doctor'])->name('doctor');
 
